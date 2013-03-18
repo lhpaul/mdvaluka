39a7b58e6d6129314399f4a10090722c0b0720be
Luis Hernan Paul <lhpaul11@gmail.com>
2013-03-18 12:34:30 -0300

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mdvaluca::Application.config.secret_token = 'ce565ffd4e541878794abfe0931535a3c656e42d55386948d231ab134bbfc72d610b1f24a1bd272cfc6437cfde81c76afe1e56bf9c8e217bd66ceb7c69af7fc7'
